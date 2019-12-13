82b4e5a71b23bbe1838a1755f01dddd3fa4be99a
kleyla <kleyla01@live.com>
2019-12-13 12:13:23 -0400

    carrito estado, lista estado

--- app/Producto.php
+++ app/Producto.php
@@ -38,38 +38,41 @@ class Producto extends Model
     {
         return $comentarios = ComentarPub::where('id_publicacion', $idp)->count();
     }
-    public static function enCarrito($id_cliente,$id_producto){
-        $producto=DB::select('SELECT productos.id,productos.nombre 
+    public static function enCarrito($id_cliente, $id_producto)
+    {
+        $producto = DB::select('SELECT productos.id,productos.nombre
         from carritoproductos, productos, clientes, carritos
         where carritoproductos.id_producto=productos.id
         and carritoproductos.id_carrito=carritos.id
+        and carritoproductos.estado = true
         and carritos.id_cliente=clientes.id
         and clientes.id=?
-        and productos.id=?',[$id_cliente,$id_producto]);
-        $empty=[];
-   
-        if($producto!==$empty){
+        and productos.id=?', [$id_cliente, $id_producto]);
+        $empty = [];
+
+        if ($producto !== $empty) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public static function enListaDeseos($id_cliente,$id_producto){
-        $producto=DB::select('SELECT productos.id,productos.nombre
+    public static function enListaDeseos($id_cliente, $id_producto)
+    {
+        $producto = DB::select('SELECT productos.id,productos.nombre
         from listaproductos, productos, clientes, listadeseos
         where listaproductos.id_producto=productos.id
         and listaproductos.id_listadeseo=listadeseos.id
         and listadeseos.id_cliente=clientes.id
+        and listaproductos.estado = true
         and clientes.id=?
-        and productos.id=?',[$id_cliente,$id_producto]);
-        $empty=[];
-   
-        if($producto!==$empty){
+        and productos.id=?', [$id_cliente, $id_producto]);
+        $empty = [];
+
+        if ($producto !== $empty) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-    
 }
