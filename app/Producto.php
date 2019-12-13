f9ead7b18a48b84b2de0a5cf173dc5987409d3a8
root <root@ip-172-26-7-9.ec2.internal>
2019-12-13 13:16:05 +0000

    Publicaciones AI update

--- app/Producto.php
+++ app/Producto.php
@@ -8,6 +8,8 @@ use App\Megusta;
 use App\MegustaPub;
 use App\CompartirPub;
 use App\ComentarPub;
+use Illuminate\Support\Facades\DB;
+
 
 class Producto extends Model
 {
@@ -36,4 +38,38 @@ class Producto extends Model
     {
         return $comentarios = ComentarPub::where('id_publicacion', $idp)->count();
     }
+    public static function enCarrito($id_cliente,$id_producto){
+        $producto=DB::select('SELECT productos.id,productos.nombre 
+        from carritoproductos, productos, clientes, carritos
+        where carritoproductos.id_producto=productos.id
+        and carritoproductos.id_carrito=carritos.id
+        and carritos.id_cliente=clientes.id
+        and clientes.id=?
+        and productos.id=?',[$id_cliente,$id_producto]);
+        $empty=[];
+   
+        if($producto!==$empty){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static function enListaDeseos($id_cliente,$id_producto){
+        $producto=DB::select('SELECT productos.id,productos.nombre
+        from listaproductos, productos, clientes, listadeseos
+        where listaproductos.id_producto=productos.id
+        and listaproductos.id_listadeseo=listadeseos.id
+        and listadeseos.id_cliente=clientes.id
+        and clientes.id=?
+        and productos.id=?',[$id_cliente,$id_producto]);
+        $empty=[];
+   
+        if($producto!==$empty){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
 }
